ec5145ad07505571ec152b057ea7ee6c87695af1
SlawCzech <slaw.czech@gmail.com>
2023-03-23 22:38:13 +0100

    models

--- users/models.py
+++ users/models.py
@@ -3,17 +3,5 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    """Custom User placeholder."""
-
-    shoe_size = models.FloatField()
-
-    def __str__(self) -> str:
-        return str(self.shoe_size)
-
-    def get_canonical_url(self) -> str:
-        """Generate canonical url for custom user.
-
-        :returns: canonical url
-        :rtype: str
-        """
-        return f"/{self.shoe_size}/canonical/"
+    """Custom user placeholder."""
+    pass
